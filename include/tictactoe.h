832d8cc986b1df029165204c6bad9231bd6c9a29
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2020-09-02 11:56:59 -0500

    created board struct

---
+++ include/tictactoe.h
@@ -0,0 +1,8 @@
+struct Board{
+    
+    int location[9] = {0,0,0,0,0,0,0,0,0};
+
+
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream> 
+#include "tictactoe.h"// this includes the board and game manager 
+
 
 int main() { 
+    
+    Board board;
 
-    std::cout<<"hello world"<<std::endl;
+    std::cout<<board.location[0]<<std::endl;
+
 
     return 0; 
 }
