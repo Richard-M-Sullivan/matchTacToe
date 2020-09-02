c071c0599a6ab8914f4267f416066eb8f0b69b1f
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2020-09-02 17:20:59 -0500

    minor modification of how things are displayed from main

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,10 @@ int main() {
     int move = 0;//create a variable to hold move selections
     int state = 0;//hold the state of the game
     
-    
+    for(int i=0; i < 5; i++){
+        std::cout<< "\n\n\n\n\n\n\n\n\n\n" <<std::endl;
+    }
+        
     while(state == 0){
         board.display();//display the board
     
@@ -26,6 +29,8 @@ int main() {
             std::cout<<"play on.!"<<std::endl;
         }
     }
+
+    std::cout<<"final board id: "<<board.getBoard()<<std::endl;
 
     if(state == 1){
         std::cout<<"player 1 wins"<<std::endl;
