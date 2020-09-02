846a10d4cdf826c9b3d565521e72689681234f13
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2020-09-02 16:31:36 -0500

    all components for tictactoe logic is complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,38 @@ int main() {
     Board board;//create the board
     int move = 0;//create a variable to hold move selections
     int state = 0;//hold the state of the game
-
-    for(int i = 0; i < 10; i ++){//for loop to test program
+    
+    
+    while(state == 0){
         board.display();//display the board
-        std::cout<<board.getBoard()<<std::endl;//print the boardCode
+    
+        std::cout<<"enter a move: #1-9 then press enter "<<std::endl;
         std::cin>>move;
+        
         board.makeMove(move);//let the player make their move
+        
         state = board.getState();
+        
+        for(int i=0; i < 5; i++){
+            std::cout<< "\n\n\n\n\n\n\n\n\n\n" <<std::endl;
+        }
+
         if(state == 0){
             std::cout<<"play on.!"<<std::endl;
         }
-        else if(state == 1){
-            std::cout<<"player 1 wins"<<std::endl;
-        }
-        else if(state == 2){
-            std::cout<<"player 2 wins"<<std::endl;
-        }
-        else if(state == -1){
-            std::cout<<"tie game.!"<<std::endl;
-        }
-        else{
-            std::cout<<"an error occured: illegal state"<<std::endl;
-        }
+    }
+
+    if(state == 1){
+        std::cout<<"player 1 wins"<<std::endl;
+    }
+    else if(state == 2){
+        std::cout<<"player 2 wins"<<std::endl;
+    }
+    else if(state == -1){
+        std::cout<<"tie game.!"<<std::endl;
+    }
+    else{
+        std::cout<<"an error occured: illegal state"<<std::endl;
     }
     return 0; 
 }
