8f1054c42baf7d823b740ed0b7345a35e982de64
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2020-10-30 21:28:37 -0500

    added allison to the author list

--- matchTacToe.py
+++ matchTacToe.py
@@ -1,5 +1,6 @@
 """
 Author: Richard M. Sullivan
+    And my partner in crime - The Great Allison Anson
 
 This is an example of how you can use the matchboxAI library in a tic tac toe
 application. I hope this inspires you to use it in creative ways that are 
