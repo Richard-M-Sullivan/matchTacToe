9ebbdc1e6d6e4bd15a1a1884c7d3e12828438975
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2021-03-14 11:05:21 -0500

    laid out program structure

--- src/matchTacToe.c
+++ src/matchTacToe.c
@@ -2,7 +2,28 @@
 
 int main(void){
 
-    printf("Hello World\n");
+    //setup stuff
+        //matchbox table
+        //game board
+        //game settings
+
+    //enter game loop
+        //display
+            //display game board
+        
+        //get input
+            //get input and validate it from the user
+            //get input and validate it from the matchbox
+
+        //update the game
+            //update the board
+            //check game state
+
+    //shutdown stuff
+        //reward/punish the matchboxes based on performance
+        //log the changes
+        //save matchbox data
+        //uninitialize data
 
     return 0;
 }
