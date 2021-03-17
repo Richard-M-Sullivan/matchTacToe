5448525f341e582f8ba95a4e5e317536131e7e65
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2021-03-16 23:07:18 -0500

    added some contents to the application functions for testing purposes

--- lib/application.c
+++ lib/application.c
@@ -4,27 +4,34 @@
 short board[ROW][COLUMN] = {EMPTY};
 
 void application_init(){
+    printf("application init: setting QUIT to false. \n\n");
     application_QUIT = FALSE;
 }
 
 void application_display() {
+    printf("application display: displaying game board. \n\n");
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COLUMN; j++) {
-            printf("%d ,%d", board[i][j],EMPTY);
+            printf("%d ", board[i][j]);
         }
+        printf("\n");
     }
-    
-    printf("\n");
-}
 
-void application_update(){
-    application_QUIT = TRUE;
+    printf("\n");
+
 }
 
 void application_getInput(){
+    printf("application getInput: \n\n");
 
 }
 
-void application_shutDown(){
 
+void application_update(){
+    printf("application update: setting quit to TRUE. \n\n");
+    application_QUIT = TRUE;
+}
+
+void application_shutDown(){
+    printf("application shutdown: \n\n");
 }
