213f42672b8f9e132916d80915a63dfabe22582b
Richard-M-Sullivan <richard.m.sullivan2@gmail.com>
2021-03-18 11:49:14 -0500

    created submenu for play menu with basic logic(incomplete)

--- lib/application.c
+++ lib/application.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <application.h>
 
+//type declarations
+typedef enum 
+{
+    PLAY_START,
+    PLAY_PLAYER_SELECT,
+    PLAY_GAME
+
+}   select_PlayMenu;
+
+
+//function definitions
 void print_MainMenu();
-void print_PlayMenu();
+void print_PlayMenu(select_PlayMenu);
 void print_OptionMenu();
 void print_TrainMenu();
 
+//variable delarations
 short board[ROW][COLUMN] = {EMPTY};
 menu menuSelect;
 char userInput;
+select_PlayMenu playMenuSelection;
 
 
-void application_init(){
+
+void application_init()
+{
     //init application variables
     application_QUIT = FALSE;
     menuSelect = MAIN;
     userInput = ' ';
+    playMenuSelection = PLAY_START;
     //init ncurses library
     initscr();
     raw();
@@ -26,18 +42,20 @@ void application_init(){
 
 
 
-void application_display() {
+void application_display() 
+{
     //clear the screen buffer
     clear();
 
     //based on the menu select draw the appropriate menu to the screen buffer
-    switch(menuSelect){
+    switch(menuSelect)
+    {
         case MAIN:
             print_MainMenu();
             break;
 
         case PLAY:
-            print_PlayMenu();
+            print_PlayMenu(playMenuSelection);
             break;
 
         case OPTION:
@@ -59,18 +77,22 @@ void application_display() {
 
 
 
-void application_getInput(){
+void application_getInput()
+{
     //grab a  character from the keyboard
     userInput = getch(); 
 }
 
 
 
-void application_update(){
+void application_update()
+{
     // apply logic based on the menu selection
-    switch(menuSelect){
+    switch(menuSelect)
+    {
         case MAIN:
-            switch(userInput){
+            switch(userInput)
+            {
                 case '1':
                     menuSelect = PLAY;
                     break;
@@ -86,15 +108,35 @@ void application_update(){
             break;
 
         case PLAY:
-            switch(userInput){
-                case '1':
-                    menuSelect = MAIN;
+            switch(playMenuSelection)
+            {
+                case PLAY_START:
+                    switch(userInput)
+                    {
+                        case '1':
+                            menuSelect = MAIN;
+                            break;
+                        case '2':
+                            playMenuSelection = PLAY_PLAYER_SELECT;
+                        case '3':
+                            playMenuSelection = PLAY_PLAYER_SELECT;
+                    }
+                    break;
+
+                case PLAY_PLAYER_SELECT:
+                    switch(userInput)
+                    {
+                        case '1':
+                            playMenuSelection = MAIN;
+                            break;
+                    }
                     break;
             }
             break;
 
         case OPTION:
-            switch(userInput){
+            switch(userInput)
+            {
                 case '1':
                     menuSelect = MAIN;
                     break;
@@ -102,7 +144,8 @@ void application_update(){
             break;
 
         case TRAIN:
-            switch(userInput){
+            switch(userInput)
+            {
                 case '1':
                     menuSelect = MAIN;
                     break;
@@ -117,40 +160,67 @@ void application_update(){
 
     //if the user inputs q set application_QUIT flag to TRUE
     // (this is used to control exiting the game loop)
-    if (userInput == 'q'){ 
+    if (userInput == 'q')
+    { 
         application_QUIT = TRUE;
     }
 }
 
 
 
-void application_shutDown(){
+void application_shutDown()
+{
     //shut down the ncurses library
     endwin();
 }
 
-void print_MainMenu(){
+void print_MainMenu()
+{
     printw("MATCH TAC TOE\n\n");
     printw("[1]: play tic tac toe\n");
     printw("[2]: options\n");
     printw("[3]: train match boxes\n\n");
 }
 
-void print_PlayMenu(){
-    printw("play menu\n\n");
-    printw("[1]: back\n");
-    printw("use the arrow keys to select a cell.\n");
-    printw("then use space bar to make your move\n");
-    printw("not implamented yet\n\n");
+void print_PlayMenu(select_PlayMenu selection)
+{
+    switch(selection)
+    {
+        case PLAY_START:
+            printw("play menu\n\n");
+            printw("[1]: back\n\n");
+            printw("select what type of game you would like to play.\n");
+            printw("[2]: one player\n");
+            printw("[3]: two player \n\n");
+            break;
+
+        case PLAY_PLAYER_SELECT:
+            printw("play menu\n\n");
+            printw("[1]: back\n\n");
+            printw("wold you like to be X or O?\n");
+            printw("[2]: X\n");
+            printw("[3]: O\n\n");
+            break;
+
+        default:
+            printw("play menu: %d\n\n",selection);
+            printw("[1]: back\n");
+            printw("use the arrow keys to select a cell.\n");
+            printw("then use space bar to make your move\n");
+            printw("not implamented yet\n\n");
+            break;
+    }
 }
 
-void print_OptionMenu(){
+void print_OptionMenu()
+{
     printw("options menu\n\n");
     printw("[1]: back\n");
     printw("no optins to display\n\n");
 }
 
-void print_TrainMenu(){
+void print_TrainMenu()
+{
     printw("train menu\n\n");
     printw("[1]: back\n");
     printw("no options to display\n\n");
